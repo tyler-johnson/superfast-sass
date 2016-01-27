f9dd4827ab1d8cec1fe3e3f33b6a37bb8b6fadcf
Tyler Johnson <tyler@vintyge.com>
2016-01-27 11:02:26 -0700

    upgraded for superfast 0.2

--- src/index.js
+++ src/index.js
@@ -1,45 +1,27 @@
 import path from "path";
 import sass from "node-sass";
-import assign from "lodash/assign";
+// import assign from "lodash/assign";
 import resolve from "resolve";
-var thispkg = require("./package.json");
 
 export default function(compile) {
-	let p = [];
-
-	// move this library to devDependency
-	let meta = compile.metadata;
-	if (meta.dependencies[thispkg.name]) {
-		let ver = meta.dependencies[thispkg.name];
-		meta.remove(thispkg.name);
-		p.push(meta.add(thispkg.name + "@" + ver, { dev: true }));
-	}
-
-	compile.transform.use(transform(compile.options.sass));
-
-	return Promise.all(p);
+	compile.transform(transform);
 }
 
-function transform(options) {
-	return function(file) {
-		if (path.extname(file.path) !== ".scss") return;
-		file.type = "style";
-		file.targets = ["client"];
-		file.originalSource = file.source;
+function transform(file, src) {
+	if (path.extname(file.path) !== ".scss") return;
+	file.setType("style");
+	file.target("client");
 
-		return new Promise((resolve, reject) => {
-			let opts = assign({}, options);
-			opts.data = file.originalSource;
-			opts.includePaths = [].concat(opts.includePaths, path.dirname(file.fullpath)).filter(Boolean);
-			opts.importer = moduleImporter;
-
-			sass.render(opts, (err, res) => {
-				if (err) return reject(err);
-				file.source = res.css.toString();
-				resolve();
-			});
+	return new Promise((resolve, reject) => {
+		sass.render({
+			data: src,
+			includePaths: [ path.dirname(file.fullpath) ],
+			importer: moduleImporter
+		}, (err, res) => {
+			if (err) return reject(err);
+			resolve(res.css.toString());
 		});
-	};
+	});
 }
 
 function moduleImporter(file, prev, done) {
